cc6ed9b34b9cbe588be26e0cf23ff05ec317b4b0
Petr Bui <buihonganh1@gmail.com>
2024-01-31 11:26:48 +0100

    Refactor template compilation and handle error cases

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import handlebars from "handlebars";
 import mjml2html from "mjml";
-import { parse, resolve } from "path";
+import { basename, join, resolve } from "path";
 
 const INPUT_DIR = "templates";
 const OUTPUT_DIR = "out";
@@ -9,29 +9,39 @@ const OUTPUT_DIR = "out";
 const getUniqueTemplatePathsWithoutExtensions = () => {
   const templates = fs.readdirSync(INPUT_DIR);
   const templatesFiles = templates
-    .filter((template) => template.includes(".mjml"))
-    .map((template) => template.split(".")[0]);
-  return templatesFiles.map((template) => resolve(`${INPUT_DIR}/${template}`));
+    .filter((template) => !template.includes(".mjml"))
+    .map((template) => `${INPUT_DIR}/${template}/${template}`);
+  return templatesFiles.map((template) => resolve(`${template}`));
 };
 
 const compileHandlebars = async (templatePaths: string[]) => {
   const mjmlWithoutHendlebars: string[][] = [];
   for (const templatePath of templatePaths) {
     const templateMjmlPath = `${templatePath}.mjml`;
-    const templateBlob = fs.readFileSync(templateMjmlPath, "utf8");
-    const { default: templateData } = await import(`${templatePath}.json`, {
-      assert: {
-        type: "json",
-      },
-    });
-    const { dir } = parse(templateMjmlPath);
+    const templateJsonPath = `${templatePath}.json`;
+    const mjmlFileExist = fs.existsSync(templateMjmlPath);
+    const jsonFileExist = fs.existsSync(templateJsonPath);
+    if (!jsonFileExist || !mjmlFileExist) {
+      if (!jsonFileExist)
+        console.error("Error: JSON file not exist in folder " + templatePath);
+      if (!mjmlFileExist)
+        console.error("Error: MJML file not exist in folder " + templatePath);
+      continue;
+    }
 
-    handlebars.registerHelper("include", (partial: string, finalProps: any) => {
-      const partialPath = resolve(dir, `${partial}.mjml`);
-      const partialBlob = fs.readFileSync(partialPath, "utf8");
-      const partialCompiled = handlebars.compile(partialBlob)(finalProps);
-      return new handlebars.SafeString(partialCompiled);
-    });
+    if (fs.readFileSync(templateJsonPath, "utf8") === "") {
+      console.error("Error: JSON file is empty in folder " + templatePath);
+      continue;
+    }
+    if (fs.readFileSync(templateMjmlPath, "utf8") === "") {
+      console.error("Error: MJML file is empty in folder " + templatePath);
+      continue;
+    }
+    const templateBlob = fs.readFileSync(join(templateMjmlPath), "utf8");
+
+    const templateData = JSON.parse(
+      fs.readFileSync(join(templateJsonPath), "utf8")
+    );
     handlebars.registerHelper("length", function (array: any) {
       return array.length;
     });
@@ -45,7 +55,7 @@ const compileHandlebars = async (templatePaths: string[]) => {
           // @ts-ignore
           return options.inverse(this);
         }
-      },
+      }
     );
     handlebars.registerHelper(
       "equals",
@@ -57,10 +67,12 @@ const compileHandlebars = async (templatePaths: string[]) => {
           // @ts-ignore
           return options.inverse(this);
         }
-      },
+      }
     );
-
-    const templateCompiled = handlebars.compile(templateBlob)(templateData);
+    const { html } = mjml2html(templateBlob, {
+      filePath: join(INPUT_DIR),
+    });
+    const templateCompiled = handlebars.compile(html)(templateData);
     mjmlWithoutHendlebars.push([templatePath, templateCompiled]);
     handlebars.unregisterHelper("include");
   }
@@ -70,11 +82,11 @@ const compileHandlebars = async (templatePaths: string[]) => {
 const main = () => {
   const templates = getUniqueTemplatePathsWithoutExtensions();
   compileHandlebars(templates).then((mjmlBlobs) => {
-    mjmlBlobs.forEach((mjmlBlob, index) => {
+    mjmlBlobs.forEach((mjmlBlob) => {
       const [templatePath, templateCompiled] = mjmlBlob;
-      const { html } = mjml2html(templateCompiled);
-      const fileName = templatePath.split("/").pop();
-      fs.writeFileSync(`${OUTPUT_DIR}/${fileName}.html`, html, "utf8");
+      const templateName = basename(`${templatePath}`, ".mjml");
+      const fileName = `${templateName}.html`;
+      fs.writeFileSync(`${OUTPUT_DIR}/${fileName}`, templateCompiled, "utf8");
     });
   });
 };
